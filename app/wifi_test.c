12b2d715a5d5c72ee72b1671b9e1a99cf643d49a
zhuangjf <zhuangjf@mxchip.com>
2015-03-23 12:01:11 +0800

    Signed-off-by: zhuangjf <zhuangjf@mxchip.com>

--- app/wifi_test.c
+++ app/wifi_test.c
@@ -1,17 +1,20 @@
 #include "mxchipWNet.h"
 #include "mico_rtos.h"
 
-int scan_end = 1;
+static int scan_end = 1;
+static int station_up = 0;
 
 void WifiStatusHandler(int event)
 {
     switch (event)
     {
     case MXCHIP_WIFI_UP:
+        station_up = 1;
         printf("wifi up\r\n");
         break;
 
     case MXCHIP_WIFI_DOWN:
+        station_up = 0;
         printf("wifi down\r\n");
         break;
 
@@ -60,8 +63,8 @@ void station_start(void)
     memset(&wNetConfig, 0, sizeof(wNetConfig));
 
     wNetConfig.wifi_mode = Station;
-    strcpy(wNetConfig.wifi_ssid, "dd-wrt");
-    strcpy(wNetConfig.wifi_key, "88888888");
+    strcpy(wNetConfig.wifi_ssid, "tao");
+    strcpy(wNetConfig.wifi_key, "39376612");
     wNetConfig.dhcpMode = DHCP_Client;
 
     StartNetwork(&wNetConfig);
@@ -78,6 +81,66 @@ void wifi_softap_and_scan_test(void *arg)
         if(scan_end)
         {
             mxchipStartScan();
+        }
+    }
+}
+
+void tcp_speed_max_test(void *arg)
+{
+    int fd = -1;
+    int ret;
+    struct sockaddr_t addr;
+    uint8_t *buffer = (uint8_t*)malloc(2048);
+    
+    printf("Enter tcp speed max Test\r\n");
+
+    station_start();
+
+    while(1)
+    {
+        if(!station_up)
+        {
+            msleep(100);
+            continue;
+        }
+
+        if(-1 == fd)
+        {
+            fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            if(-1 == fd)
+            {
+                sleep(1);
+                continue;
+            }
+
+            memset(&addr, 0, sizeof(addr));
+            addr.s_ip = inet_addr("192.168.1.104");
+            addr.s_port = 8080;
+
+            ret = connect(fd, &addr, sizeof(addr));
+            if(-1 == ret)
+            {
+                close(fd);
+                fd = -1;
+                msleep(100);
+                continue;
+            }
+        }
+        else
+        {
+            ret = send(fd, buffer, 2048, 0);
+            if(-1 == ret)
+            {
+                printf("send failed, to rebuild\n");
+                close(fd);
+                fd = -1;
+                msleep(100);
+                continue;
+            }
+            else
+            {
+                printf("send ok\r\n");
+            }
         }
     }
 }
@@ -100,7 +163,8 @@ void test_thread_init(mico_thread_t *handler, uint8_t cmd)
             printf("wifi_softap_and_scan\r\n");
             break;
         case 22:
-            printf("wifi_softap_and_scan\r\n");
+            mico_rtos_create_thread(handler,MICO_APPLICATION_PRIORITY,"tcp_speed_max",
+                tcp_speed_max_test,0x400,NULL);
             break;
         case 23:
             printf("wifi_softap_and_scan\r\n");
